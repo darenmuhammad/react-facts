311d082c7c80a2625395332609ffc1956cd60878
darenmuhammad <darenmuhammad10@gmail.com>
2025-06-18 10:03:06 +0700

    finished

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/react-logo.png";
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src={logo} alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    );
+}
